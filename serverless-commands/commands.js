4603ca6f8817c040ba8ff12bea177f3fef6d88a5
Zak <zakattack26111@gmail.com>
2017-11-13 09:27:53 -1000

    Add files via upload

--- serverless-commands/commands.js
+++ serverless-commands/commands.js
@@ -3,6 +3,8 @@
 
 // testing lambda functions
 // serverlesss invoke local -f function -p test.json
+// OR
+// sls invoke local -f "function name"
 // *In the command above, "function" is the name of the function to run, and "test.json" is the JSON file of which the data is put in
 
 // creating a bucket in aws
@@ -19,3 +21,21 @@
 
 // publish page
 // curl http://test-bucket-zak.s3-websites-us-west-2.amazonaws.com
+
+// npm init --yes
+// creates package.json
+
+// npm install -s aws-sdk
+// installs aws-sdk (make sure to add node_modules to .gitignore)
+
+// sls invoke local -f function-name --path serverless.yml
+// runs lambda function from local drive
+
+// aws s3 website s3://zak-dev-serverlessdeploymentbucket-179rc2ts8tuat/ --index-document index.html --error-document error.html
+// changes bucket to static webstie and also indicates to run off index.html
+
+// aws s3 cp /c/Users/Student/Documents/Zak-Capstone/aws-photo-bucket/client-side/index.html s3://zak-dev-serverlessdeploymentbucket-179rc2ts8tuat/ --acl public-read
+// uploads index.html to aws bucket
+
+// http://zak-dev-serverlessdeploymentbucket-179rc2ts8tuat.s3-website-us-west-2.amazonaws.com/
+// link to website, follow format of: http://bucketname.region.amazonaws.com
